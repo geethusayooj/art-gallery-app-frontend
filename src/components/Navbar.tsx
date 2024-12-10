f0c91c69fbd98ebb2571d55da7132d748458c22f
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-12-10 12:19:54 +0100

    add search functionality and background image for all pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,24 @@ import "./Navbar.css";
 interface NavbarProps {
   onSearch: (searchTerm: string) => void; 
 }
-function Navbar({ onSearch }: NavbarProps) {
 
+function Navbar({ onSearch }: NavbarProps) {
   const [selectedTab, setSelectedTab] = useState<string>("/");
   const [searchInput, setSearchInput] = useState<string>("");
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
-    setSelectedTab(location.pathname);
+    const path = location.pathname;
+    if (path.startsWith("/artist")) {
+      setSelectedTab("/artists");
+    } else if (path.startsWith("/create-artwork")) {
+      setSelectedTab("/create-artwork");
+    } else if (path === "/about") {
+      setSelectedTab("/about");
+    } else {
+      setSelectedTab("/"); 
+    }
   }, [location.pathname]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,6 +50,7 @@ function Navbar({ onSearch }: NavbarProps) {
         <Tab value="/" label="Home" classes={{ root: "tab" }} />
         <Tab value="/about" label="About" classes={{ root: "tab" }} />
         <Tab value="/artists" label="Create" classes={{ root: "tab" }} />
+        
       </Tabs>
 
       <form onSubmit={handleSearchSubmit} className="search-form">
