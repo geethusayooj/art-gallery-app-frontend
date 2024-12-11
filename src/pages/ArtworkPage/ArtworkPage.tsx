97568b49f481350e5f0091603bfd76e5cfefffa3
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-12-11 16:59:07 +0100

    fix build error

--- src/pages/ArtworkPage/ArtworkPage.tsx
+++ src/pages/ArtworkPage/ArtworkPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { useParams, useNavigate } from "react-router-dom";
 import "./ArtworkPage.css";
 import axios from "axios";
